7e74865a5b6d6a6b2d27a9c4a17d3b677650f990
Cameron <camcreel@gmail.com>
2021-12-01 19:47:38 -0500

    day 1 challenge 2

--- day1.rb
+++ day1.rb
@@ -14,3 +14,16 @@ while i < depths.length
 end
 
 puts total
+
+total = 0
+i = 3
+window1 = depths[0] + depths[1] + depths[3]
+while i < depths.length
+    new_window = window1 + depths[i] - depths[i-3]
+    if ( new_window > window1)
+        total += 1
+    end
+    window1 = new_window
+    i += 1
+end
+puts total
